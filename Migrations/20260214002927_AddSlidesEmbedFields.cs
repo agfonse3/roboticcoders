370afe02de6722e46443c34215fca7f2797e2f43
Giselle <giselle@bacongames.co>
2026-02-16 09:02:30 -0500

    update

---
+++ Migrations/20260214002927_AddSlidesEmbedFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoboticCoders.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSlidesEmbedFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SlidesEmbedUrl",
+                table: "Lessons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SlidesType",
+                table: "Lessons",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SlidesEmbedUrl",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "SlidesType",
+                table: "Lessons");
+        }
+    }
+}
